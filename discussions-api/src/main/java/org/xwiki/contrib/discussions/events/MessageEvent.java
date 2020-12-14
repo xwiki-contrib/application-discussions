22da9901bde473036b8a1ae636f30aeffce401e0
Manuel Leduc <manuel.leduc@xwiki.com>
2020-12-14 10:51:44 +0100

    [misc] add the equals operations on MessageEvent.

--- discussions-api/src/main/java/org/xwiki/contrib/discussions/events/MessageEvent.java
+++ discussions-api/src/main/java/org/xwiki/contrib/discussions/events/MessageEvent.java
@@ -19,6 +19,8 @@
  */
 package org.xwiki.contrib.discussions.events;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.xwiki.stability.Unstable;
 
 /**
@@ -55,5 +57,31 @@ public class MessageEvent implements DiscussionsEvent
     {
         return otherEvent instanceof MessageEvent
             && this.actionType == ((MessageEvent) otherEvent).actionType;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        MessageEvent that = (MessageEvent) o;
+
+        return new EqualsBuilder()
+            .append(this.actionType, that.actionType)
+            .isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(17, 37)
+            .append(this.actionType)
+            .toHashCode();
     }
 }
