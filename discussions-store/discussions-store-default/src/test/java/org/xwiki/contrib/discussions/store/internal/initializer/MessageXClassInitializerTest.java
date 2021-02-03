adb4b4fe617c5da0fab3f3e034ab869b92df7089
Manuel Leduc <manuel.leduc@xwiki.com>
2021-02-03 14:54:01 +0100

    [misc] Fix package declarations badly placed before the headers.

--- discussions-store/discussions-store-default/src/test/java/org/xwiki/contrib/discussions/store/internal/initializer/MessageXClassInitializerTest.java
+++ discussions-store/discussions-store-default/src/test/java/org/xwiki/contrib/discussions/store/internal/initializer/MessageXClassInitializerTest.java
@@ -1,4 +1,4 @@
-package org.xwiki.contrib.discussions.store.internal.initializer;/*
+/*
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
  *
@@ -17,6 +17,7 @@ package org.xwiki.contrib.discussions.store.internal.initializer;/*
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+package org.xwiki.contrib.discussions.store.internal.initializer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
